e8baff9fdc688a596deeeb2bc75c5f526675dc46
huhsway <seunghoe9542@naver.com>
2025-10-09 16:51:37 +0900

    20251009

---
+++ src/week1/TwoSum.java
@@ -0,0 +1,23 @@
+package week1;
+
+import java.util.*;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[] {hashMap.get(target - nums[i]), i};
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
