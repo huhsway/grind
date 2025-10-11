5f75337e316a3a49ef394202b38179d078b9d807
huhsway <seunghoe9542@naver.com>
2025-10-11 18:43:48 +0900

    20251011

---
+++ src/week2/Solution.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        long left = 1;
+        long right = n;
+        long firstBad = n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (isBadVersion((int) mid)) {
+                firstBad = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return (int) firstBad;
+    }
+}
