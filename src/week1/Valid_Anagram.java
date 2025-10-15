2ab58a6faa18d12cc4203826106569d5e5b19c36
huhsway <seunghoe9542@naver.com>
2025-10-15 22:15:49 +0900

    20251015

--- src/week1/Valid_Anagram.java
+++ src/week1/Valid_Anagram.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
         }
 
-        int[] charCounts = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            charCounts[s.charAt(i) - 'a']++;
-            charCounts[t.charAt(i) - 'a']--;
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
 
-        for (int count : charCounts) {
-            if (count != 0) {
-                return false;
-            }
-        }
+        // 만약 출력 하려면
+        // sMap.forEach((key, value) -> System.out.println(key + ": " + value));
 
-        return true;
+        // return Arrays.equals(sMap.entrySet().toArray(), tMap.entrySet().toArray());
+
+    // 두 맵을 비교하는 올바른 방법입니다.
+    // 맵의 크기가 같고, 모든 키-값 쌍이 동일한지 확인하며,
+    // 요소의 삽입 순서와는 상관없이 올바르게 비교합니다.
+         return sMap.equals(tMap);
     }
 }
