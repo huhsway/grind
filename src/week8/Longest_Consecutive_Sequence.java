776081696d8e48563e0ddf8b8da892ddfea99915
huhsway <seunghoe9542@naver.com>
2025-11-02 10:36:57 +0000

    20251102

---
+++ src/week8/Longest_Consecutive_Sequence.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    /**
+     * LeetCode 128. Longest Consecutive Sequence 문제의 O(n) 풀이
+     * HashSet을 사용하여 연속된 시퀀스의 길이를 찾습니다.
+     *
+     * @param nums 정렬되지 않은 정수 배열
+     * @return 가장 긴 연속된 요소 시퀀스의 길이
+     */
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // 1. 모든 요소를 HashSet에 추가 (중복 제거 및 O(1) 탐색 보장)
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        // 2. Set의 각 요소를 순회하며 시퀀스 시작점을 찾고 길이를 측정
+        for (int num : numSet) {
+            
+            // 현재 숫자(num)가 시퀀스의 시작점인지 확인
+            // -> num - 1 이 Set에 없으면 num이 시퀀스의 시작점입니다.
+            if (!numSet.contains(num - 1)) {
+                
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // 시퀀스 확장: currentNum + 1, + 2, ... 를 Set에서 찾습니다.
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                // 최대 길이 업데이트
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
---
+++ src/week8/Rotate_Array.java
@@ -0,0 +1,52 @@
+package LeetCode.Array;
+
+import java.util.Arrays;
+
+// 네이버 면접에서도 나왔음
+public class Solution {
+
+    // public int[] rotate(int[] nums, int k) {
+
+    //     int n = nums.length - 1;
+    //     for (int i = 0; i < k; i++) {
+
+    //         int last = nums[n];
+
+    //         for (int j = n; j > 0; j--) {
+    //             nums[j] = nums[j-1];
+    //         }
+
+    //         nums[0] = last;
+
+    //     }
+    //     return nums;
+    // }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+
+        // 보조 배열 생성
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            // 원본 배열의 i번째 요소가 이동할 새로운 위치를 계산
+            int newIndex = (i + k) % n;
+            result[newIndex] = nums[i];
+        }
+
+        // 결과를 원본 배열로 복사
+        for (int i = 0; i < n; i++) {
+            nums[i] = result[i];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int t = 3;
+        RotateArray solution = new RotateArray();
+        System.out.println(Arrays.toString(solution.rotateArray(arr, t)));
+    }
+
+}
