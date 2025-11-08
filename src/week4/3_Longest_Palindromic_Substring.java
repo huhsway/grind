638ae9c01386c3dc8d44558f6e21469f034c36d4
huhsway <seunghoe9542@naver.com>
2025-11-08 09:24:43 +0000

    251108

--- src/week4/3_Longest_Palindromic_Substring.java
+++ src/week4/3_Longest_Palindromic_Substring.java
@@ -1,47 +1,47 @@
-// https://leetcode.com/problems/longest-palindromic-substring/
 public class Solution {
-    private boolean isPalindrome(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        return s.equals(rev);
+    
+    // 중심에서 양쪽으로 확장하는 헬퍼 함수
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // 팰린드롬의 길이 반환
+        return right - left - 1;
     }
-
+    
     public String longestPalindrome(String s) {
-        int n = s.length();
-
-        if(isPalindrome(s)) return s;
-
-        int maxLength = 1;
+        if (s == null || s.length() < 1) return "";
+        
         int start = 0;
-        int left, right;
-
-        for (int i = 0; i < n; i++) {
-            left = i - 1;
-            right = i + 1;
-
-            while (right < n && s.charAt(right) == s.charAt(i)) // increment 'right'
-                right++;
-
-            while (left >= 0 && s.charAt(left) == s.charAt(i)) // decrement 'left'
-                left--;
-
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-            }
-
-            if (maxLength < right - left - 1) {
-                maxLength = right - left - 1;
-                start = left + 1;
+        int maxLength = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // 홀수 길이 팰린드롬 (중심이 한 글자)
+            int len1 = expandAroundCenter(s, i, i);
+            
+            // 짝수 길이 팰린드롬 (중심이 두 글자)
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            // 둘 중 더 긴 것 선택
+            int len = Math.max(len1, len2);
+            
+            // 최대 길이 업데이트
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
             }
         }
-
+        
         return s.substring(start, start + maxLength);
     }
-
+    
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "babad";
-        String result = solution.longestPalindrome(s);
-        System.out.println("Longest palindrome substring is: " + result);
+        
+        System.out.println(solution.longestPalindrome("babad"));   // "bab" or "aba"
+        System.out.println(solution.longestPalindrome("cbbd"));    // "bb"
+        System.out.println(solution.longestPalindrome("abbba"));   // "abbba"
+        System.out.println(solution.longestPalindrome("aabbaa"));  // "aabbaa"
     }
 }
