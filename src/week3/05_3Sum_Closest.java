af3ba95457b09fe26398c7634dfc43b8ad444499
huhsway <seunghoe9542@naver.com>
2025-11-29 04:45:48 +0000

    251129

--- src/week3/05_3Sum_Closest.java
+++ src/week3/05_3Sum_Closest.java
@@ -28,17 +28,17 @@ class Solution {
                     // 정확히 target과 같으면 즉시 반환
                     return sum;
                 } else if (sum < target) {
-                    left++;
                     // 중복 제거: left
-                    while (left < right && nums[left] == nums[left - 1]) {
+                    while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
+                    left++;
                 } else {
-                    right--;
                     // 중복 제거: right
-                    while (left < right && nums[right] == nums[right + 1]) {
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
+                    right--;
                 }
             }
         }
