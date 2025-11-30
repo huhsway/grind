3e5fae716bd59b0dd0ec70ce192ae8093de80a6c
huhsway <seunghoe9542@naver.com>
2025-11-30 10:23:38 +0000

    251130

--- src/week6/4_Median_of_Two_Sorted_Arrays.java
+++ src/week6/4_Median_of_Two_Sorted_Arrays.java
@@ -1,68 +1,56 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // 성능 최적화를 위해 항상 nums1을 더 짧거나 같은 길이의 배열로 만듭니다.
+        // nums1이 더 짧은 배열이 되도록 swap (이진탐색 범위 최소화)
         if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        // 배열에서 자를 갯수가 필요하기 때문에 lengt-1이 아닌 length를 사용합니다.
         int m = nums1.length;
         int n = nums2.length;
         
-        // 이진 탐색 범위 설정: nums1의 파티션 인덱스 i
+        // nums1에서 이진탐색 (0개 ~ m개 선택 가능)
         int low = 0;
         int high = m;
         
-        // 전체 원소의 절반 개수 (중앙값 위치)
-        // (m + n + 1) / 2를 사용하여 N이 홀수일 때 (N+1)/2, 짝수일 때 N/2가 되도록 합니다.
+        // 왼쪽 파티션에 들어갈 원소 개수
+        // +1을 해서 홀수일 때 중앙값이 왼쪽에 포함되도록 함
         int halfLen = (m + n + 1) / 2;
 
         while (low <= high) {
-            // nums1의 파티션 인덱스 i
+            // i: nums1에서 왼쪽에 넣을 개수
             int i = low + (high - low) / 2;
             
-            // nums2의 파티션 인덱스 j (i + j = halfLen을 만족하도록 자동 계산)
+            // j: nums2에서 왼쪽에 넣을 개수 (i + j = halfLen)
             int j = halfLen - i;
 
-            // --- 경계값 처리 및 유효성 검사 ---
-            
-            // nums1의 왼쪽 최댓값 (i=0이면 -INF, 아니면 nums1[i-1])
+            // 경계 처리: 배열 범위 벗어나면 극값 사용
             int maxLeft1 = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
-            // nums1의 오른쪽 최솟값 (i=m이면 +INF, 아니면 nums1[i])
             int minRight1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
-            
-            
-            // nums2의 왼쪽 최댓값 (j=0이면 -INF, 아니면 nums2[j-1])
             int maxLeft2 = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
-            // nums2의 오른쪽 최솟값 (j=n이면 +INF, 아니면 nums2[j])
             int minRight2 = (j == n) ? Integer.MAX_VALUE : nums2[j];
             
-            // --- 파티션 조건 검사 ---
-            
-            // 최적의 파티션을 찾은 경우: max(왼쪽) <= min(오른쪽)
+            // 올바른 파티션 찾음: 왼쪽 max <= 오른쪽 min
             if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                // 왼쪽 부분의 최댓값 (L)
                 int maxLeft = Math.max(maxLeft1, maxLeft2);
                 
-                // 전체 길이가 홀수인 경우 (maxLeft가 중앙값)
+                // 홀수: 왼쪽 최댓값이 중앙값
                 if ((m + n) % 2 == 1) {
                     return maxLeft;
                 }
                 
-                // 전체 길이가 짝수인 경우 (maxLeft와 minRight의 평균이 중앙값)
+                // 짝수: 왼쪽 최댓값과 오른쪽 최솟값의 평균
                 int minRight = Math.min(minRight1, minRight2);
                 return (double)(maxLeft + minRight) / 2.0;
 
             } else if (maxLeft1 > minRight2) {
-                // nums1의 왼쪽 부분이 너무 크므로 i를 줄여야 함
+                // nums1에서 너무 많이 가져옴 → i 줄이기
                 high = i - 1;
-            } else { // maxLeft2 > minRight1
-                // nums1의 왼쪽 부분이 너무 작으므로 i를 늘려야 함
+            } else {
+                // nums1에서 너무 적게 가져옴 → i 늘리기
                 low = i + 1;
             }
         }
 
-        // 이 코드는 항상 유효한 입력을 가정하므로 도달하지 않아야 합니다.
         return 0.0;
     }
 }
