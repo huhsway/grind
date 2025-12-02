3f308388c98d162ea2abd992d8a3f7870b338b84
huhsway <seunghoe9542@naver.com>
2025-12-02 12:07:10 +0000

    251202

--- src/week7/4_Number_of_Islands.java
+++ src/week7/4_Number_of_Islands.java
@@ -4,40 +4,40 @@ class Solution {
             return 0;
         }
 
-        int numRows = grid.length;
-        int numCols = grid[0].length;
+        int r = grid.length;
+        int c = grid[0].length;
         int numIslands = 0;
 
         // 그리드의 모든 셀을 순회합니다.
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numCols; c++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 // '1' (육지)을 발견하면, 새로운 섬을 찾은 것입니다.
-                if (grid[r][c] == '1') {
+                if (grid[i][j] == '1') {
                     numIslands++;
                     // 해당 섬과 연결된 모든 육지를 '0'으로 변경하여 재탐색을 방지합니다.
-                    dfs(grid, r, c);
+                    dfs(grid, i, j);
                 }
             }
         }
         return numIslands;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        int numRows = grid.length;
-        int numCols = grid[0].length;
+    private void dfs(char[][] grid, int y, int x) {
+        int r = grid.length;
+        int c = grid[0].length;
 
         // 경계 조건을 벗어나거나, '0'(물)인 경우 종료합니다.
-        if (r < 0 || r >= numRows || c < 0 || c >= numCols || grid[r][c] == '0') {
+        if (y < 0 || y >= r || x < 0 || x >= c || grid[y][x] == '0') {
             return;
         }
 
         // 현재 위치를 '0'으로 바꿔 방문했음을 표시합니다.
-        grid[r][c] = '0';
+        grid[y][x] = '0';
 
         // 상하좌우 인접한 셀을 재귀적으로 탐색합니다.
-        dfs(grid, r - 1, c); // 상
-        dfs(grid, r + 1, c); // 하
-        dfs(grid, r, c - 1); // 좌
-        dfs(grid, r, c + 1); // 우
+        dfs(grid, y - 1, x); // 상
+        dfs(grid, y + 1, x); // 하
+        dfs(grid, y, x - 1); // 좌
+        dfs(grid, y, x + 1); // 우
     }
 }
--- src/week7/5_Rotting_Oranges.java
+++ src/week7/5_Rotting_Oranges.java
@@ -7,14 +7,14 @@ class Solution {
             return 0;
         }
 
-        int R = grid.length;
-        int C = grid[0].length;
+        int r = grid.length;
+        int c = grid[0].length;
         Queue<int[]> queue = new ArrayDeque<>();
         int freshOranges = 0;
 
         // 1. 초기 상태 설정: 썩은 오렌지(2)를 큐에 넣고 신선한 오렌지(1)의 개수를 셉니다.
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 if (grid[i][j] == 2) {
                     queue.offer(new int[]{i, j});
                 } else if (grid[i][j] == 1) {
@@ -25,25 +25,25 @@ class Solution {
 
         // 2. BFS 시작: 시간이 0분부터 시작합니다.
         int minutes = 0;
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while (!queue.isEmpty() && freshOranges > 0) {
             // 현재 분에 썩은 모든 오렌지들을 처리합니다.
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] orange = queue.poll();
-                int r = orange[0];
-                int c = orange[1];
+                int ny = orange[0];
+                int nx = orange[1];
 
                 // 인접한 오렌지들을 탐색합니다.
-                for (int[] dir : directions) {
-                    int newR = r + dir[0];
-                    int newC = c + dir[1];
+                for (int[] dir : dirs) {
+                    int dy = ny + dir[0];
+                    int dx = nx + dir[1];
 
                     // 유효성 검사: 경계 내에 있고, 신선한 오렌지인 경우
-                    if (newR >= 0 && newR < R && newC >= 0 && newC < C && grid[newR][newC] == 1) {
-                        grid[newR][newC] = 2; // 신선한 오렌지를 썩게 만듭니다.
-                        queue.offer(new int[]{newR, newC});
+                    if (dy >= 0 && dy < r && dx >= 0 && dx < c && grid[dy][dx] == 1) {
+                        grid[dy][dx] = 2; // 신선한 오렌지를 썩게 만듭니다.
+                        queue.offer(new int[]{dy, dx});
                         freshOranges--; // 신선한 오렌지 개수를 감소시킵니다.
                     }
                 }
