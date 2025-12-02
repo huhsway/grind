62f9fd6fd9fc819914d04aba20af8b7f05ee65ce
huhsway <seunghoe9542@naver.com>
2025-12-02 14:10:10 +0000

    251202

--- src/week7/6_Accounts_Merge.java
+++ src/week7/6_Accounts_Merge.java
@@ -1,12 +1,9 @@
 import java.util.*;
 
 class Solution {
-    Map<String, String> parent;
-    Map<String, String> emailToName;
-
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        parent = new HashMap<>();
-        emailToName = new HashMap<>();
+        Map<String, String> parent = new HashMap<>();
+        Map<String, String> emailToName = new HashMap<>();
 
         // 1. 초기화: 모든 이메일을 자기 자신의 부모로 설정
         for (List<String> account : accounts) {
@@ -22,16 +19,15 @@ class Solution {
         for (List<String> account : accounts) {
             String firstEmail = account.get(1);
             for (int i = 2; i < account.size(); i++) {
-                union(firstEmail, account.get(i));
+                union(parent, firstEmail, account.get(i));
             }
         }
 
         // 3. 그룹화: 같은 루트를 가진 이메일들을 모음
         Map<String, List<String>> mergedAccounts = new HashMap<>();
         for (String email : parent.keySet()) {
-            String root = find(email);
-            mergedAccounts.putIfAbsent(root, new ArrayList<>());
-            mergedAccounts.get(root).add(email);
+            String root = find(parent, email);
+            mergedAccounts.computeIfAbsent(root, k -> new ArrayList<>()).add(email);
         }
 
         // 4. 결과 정리: 정렬하고 이름 추가
@@ -47,19 +43,18 @@ class Solution {
         return result;
     }
 
-    private String find(String s) {
+    private String find(Map<String, String> parent, String s) {
         if (parent.get(s).equals(s)) {
             return s;
         }
-        // 경로 압축 (Path Compression)
-        String root = find(parent.get(s));
+        String root = find(parent, parent.get(s));
         parent.put(s, root);
         return root;
     }
 
-    private void union(String s1, String s2) {
-        String root1 = find(s1);
-        String root2 = find(s2);
+    private void union(Map<String, String> parent, String s1, String s2) {
+        String root1 = find(parent, s1);
+        String root2 = find(parent, s2);
         if (!root1.equals(root2)) {
             parent.put(root2, root1);
         }
