22583d6ee8f17726b58ac52a45526e95f950b979
huhsway <seunghoe9542@naver.com>
2025-12-09 12:54:09 +0000

    251209

--- src/week8/031_Same_Tree.java
+++ src/week8/031_Same_Tree.java
@@ -1,21 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // 둘 다 null이면 같은 트리
+
         if (p == null && q == null) {
             return true;
         }
-        
-        // 하나만 null이면 다른 트리
+
         if (p == null || q == null) {
             return false;
         }
-        
-        // 현재 노드의 값이 다르면 다른 트리
+
         if (p.val != q.val) {
             return false;
         }
-        
-        // 왼쪽 서브트리와 오른쪽 서브트리가 모두 같아야 함
+
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
     }
 }
